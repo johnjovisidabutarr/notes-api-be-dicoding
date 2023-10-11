d0434465c358153a077537779d31eefbb64fb3d2
johnjovisidabutarr <johnjovisidabutar@student.telkomuniversity.ac.id>
2023-10-11 13:25:16 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -7,11 +8,17 @@ const NotesValidator = require('./validator/notes');
 
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
-const UserValidator = require('./validator/users');
+const UsersValidator = require('./validator/users');
+
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -22,6 +29,29 @@ const init = async () => {
       },
     },
   });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
   await server.register([
     {
       plugin: notes,
@@ -34,10 +64,20 @@ const init = async () => {
       plugin: users,
       options: {
         service: usersService,
-        validator: UserValidator,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
